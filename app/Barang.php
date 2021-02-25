05f20d1980b644735865c9523f7d6675cc2d583a
ilahaka77 <ahmadarifilahaka77@gmail.com>
2021-02-25 16:00:25 +0700

    fix modal barang

--- app/Barang.php
+++ app/Barang.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     protected $fillable = ['nama_barang', 'barcode', 'kategori_id', 'merek', 'stok', 'diskon', 'harga_beli', 'harga_jual'];
+
+    public function getDiskonAttribute($value)
+    {
+        return (float) $this->diskon;
+    }
 }
