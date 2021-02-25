c4da50b0a995158f674b4893ced4bea0be1e1a76
ilahaka77 <ahmadarifilahaka77@gmail.com>
2021-02-25 13:38:15 +0700

    fix seeder barang

--- database/seeds/BarangSeeder.php
+++ database/seeds/BarangSeeder.php
@@ -13,8 +13,29 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Barang::class, 50)->create();
+        
+        DB::table('barangs')->insert([
+            [
+                'barcode' => $this->randomNumber(10),
+                'nama_barang' => Str::random(10),
+                'kategori_id' => rand(1,3),
+                'merek' => Str::random(5),
+                'stok' => rand(1,99),
+                'diskon' => mt_rand(0,100)/100,
+                'harga_beli' => rand(100000, 100000000),
+                'harga_jual' => rand(100000, 100000000),
+            ]
+        ]);
     }
 
-    
+    public function randomNumber($length)
+    {
+        $result = '';
+
+        for($i=0; $i<$length; $i++){
+            $result .= mt_rand(0,9);
+        }
+
+        return $result;
+    }
 }
