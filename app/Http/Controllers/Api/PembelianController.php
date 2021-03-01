dc3f18cc4ca93b55a2749c18cb8fcf327aaec262
ilahaka77 <ahmadarifilahaka77@gmail.com>
2021-03-01 08:46:33 +0700

    update

--- app/Http/Controllers/Api/PembelianController.php
+++ app/Http/Controllers/Api/PembelianController.php
@@ -4,12 +4,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PembelianController extends Controller
 {
     
     public function index()
     {
-        // $pembelian = DB
+        $pembelian = DB::table('pembelians')
+            ->select('pembelians.created_at as tanggal', 'suppliers.nama_supplier', 'pembelians.total_bayar')
+            ->join('suppliers', 'pembelians.supplier_id', '=', 'suppliers.id')
+            ->get();
+
+        return response()->json([
+            'data' => $pembelian
+        ],200);
     }
 }
